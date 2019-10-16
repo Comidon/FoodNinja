6265ccc9871f4ed36390a677471db648c5a47b94
u1274265 <u1274265@utah.edu>
2019-10-16 12:44:52 -0600

    Rewrite event handler

---
+++ Assets/Scripts/MyInteractTouch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public struct ObjectInteractEventArgs
+{
+    public VRTK_ControllerReference controllerReference;
+    public GameObject target;
+}
+
+public delegate void ObjectInteractEventHandler(object sender, ObjectInteractEventArgs e);
+
+[AddComponentMenu("VRTK/Scripts/Interactions/Interactors/VRTK_InteractTouch")]
+
+
+
+public class MyInteractTouch : VRTK_InteractTouch
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
