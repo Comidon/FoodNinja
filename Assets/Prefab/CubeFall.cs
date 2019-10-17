e6b111794a718f015959b60f82b42e35448cb333
tali97 <335271710@qq.com>
2019-10-17 10:58:30 -0600

    cube falling

---
+++ Assets/Prefab/CubeFall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFall : MonoBehaviour
+{
+    public float delay = 0.3f;
+    public GameObject cube1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawning", delay, delay);
+    }
+
+    void Spawning()
+    {
+        Instantiate(cube1, new Vector3(Random.Range(-6, 6), Random.Range(-10, 10), 10), Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
