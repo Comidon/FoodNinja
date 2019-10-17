d062716a4759a343fd06141146a2392f24d6cad2
Yuhan-Wu <54376003+Yuhan-Wu@users.noreply.github.com>
2019-10-16 21:03:09 -0600

    Try vrtk scripts

--- Assets/Scripts/MyInteractTouch.cs
+++ Assets/Scripts/MyInteractTouch.cs
@@ -1,30 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using VRTK;
 
-public struct ObjectInteractEventArgs
-{
-    public VRTK_ControllerReference controllerReference;
-    public GameObject target;
-}
-
-public delegate void ObjectInteractEventHandler(object sender, ObjectInteractEventArgs e);
-
 [AddComponentMenu("VRTK/Scripts/Interactions/Interactors/VRTK_InteractTouch")]
-
-
-
 public class MyInteractTouch : VRTK_InteractTouch
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnControllerStartTouchInteractableObject(ObjectInteractEventArgs e)
     {
+        base.OnControllerTouchInteractableObject(e);
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Console.WriteLine("get hit");
     }
 }
---
+++ Assets/Scripts/MyInteractableObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using VRTK;
+
+public class MyInteractableObject : VRTK_InteractableObject
+{
+    public override void OnInteractableObjectGrabbed(InteractableObjectEventArgs e)
+    {
+        base.OnInteractableObjectGrabbed(e);
+        Console.WriteLine("Grabbed");
+    }
+}
