320a55e1e5e74a0f7982a41ea57f6116d3b4d9d1
tali97 <335271710@qq.com>
2019-10-19 13:20:35 -0600

    falling mechanic

--- Assets/Prefab/CubeFall.cs
+++ Assets/Prefab/CubeFall.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class CubeFall : MonoBehaviour
 {
-    public float delay = 0.3f;
+    public float delaySpawn = 1f;
+    public float delayBreak = 0.3f;
     public GameObject cube1;
+    public GameObject cylinder1;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawning", delay, delay);
+        InvokeRepeating("Spawning", delaySpawn, delayBreak);
     }
 
     void Spawning()
     {
-        Instantiate(cube1, new Vector3(Random.Range(-6, 6), Random.Range(-10, 10), 10), Quaternion.identity);
+        Instantiate(cube1, new Vector3(Random.Range(-6, 6), 20, Random.Range(-10, 10)), Quaternion.identity);
+        Instantiate(cylinder1, new Vector3(Random.Range(-6, 6), 20, Random.Range(-10, 10)), Quaternion.identity);
     }
     // Update is called once per frame
     void Update()
