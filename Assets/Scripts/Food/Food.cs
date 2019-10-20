49877b5e3fa47feeb04da9ac92bcb44ba0084fe0
Don <comidon@outlook.com>
2019-10-19 21:40:19 -0600

    Build a test scene

---
+++ Assets/Scripts/Food/Food.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+
+public class Food : InteractionBehaviour
+{
+    public Assets.Scripts.Utilities.FoodType type;
+    public Nutrition nutrition { get; protected set; }
+}
