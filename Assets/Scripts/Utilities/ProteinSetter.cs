b7b983e9ea6ec6cac33eb09e475d8f87146fcb6f
Yuhan-Wu <54376003+Yuhan-Wu@users.noreply.github.com>
2019-10-23 22:10:46 -0600

    achievement value setter

---
+++ Assets/Scripts/Utilities/ProteinSetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProteinSetter : AchievementValueSetter
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float total = DataToAchievement.Expected_Default.Protein;
+        float max = DataToAchievement.Max_Default.Protein;
+
+        setBar(total, max);
+        setTotal(total);
+        setMaximum(max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
