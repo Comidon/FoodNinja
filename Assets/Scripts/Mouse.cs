8c8d4e957b5dc064cb553934a0c8f388be671f08
Don <comidon@outlook.com>
2019-10-30 11:54:47 -0600

    Add eat script

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+
+public class Mouse : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            if (other.gameObject.GetComponent<InteractionBehaviour>().isGrasped)
+            {
+                NutritionManager.instance.addUpNutritionAmount(other.gameObject.GetComponent<Food>().nutrition);
+
+                // TODO: Play eat sound
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
